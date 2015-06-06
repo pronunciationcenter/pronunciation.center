59a4c15cfc50424cc34117e5ee692fb85289b562
Waldeilson Santos <pronunciation.center@gmail.com>
2015-06-06 02:17:16 -0400

    0.0.0
    Initial Version

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config{
+
+    //Database settings
+    public $dbhost    = "localhost";
+    public $dbname    = "pronunciationDB";
+    public $dbuser    = "root";
+    public $dbpass    = "";
+
+    //Website settings
+    public $wwwroot   = "http://localhost/pronunciation.center/";
+}
+?>
---
+++ suggestions.php
@@ -0,0 +1,36 @@
+<?php
+	require_once ('./menu.php');
+?>
+<!doctype html>
+<html lang="en">
+	<?php include "header.html";?>
+<body>
+	<?php include_once("analyticstracking.php") ?>
+	<!-- Header -->
+	<div id="header-wrapper" class="container">
+		<?php 
+			$menu = new menu();
+			$menu->getMenu("suggestions");
+		?>
+	</div>
+	
+	<!-- Main -->
+	<div class="wrapper container">
+		<main class="">
+			<article>
+				<header>
+					<h1>Suggested links</h1>
+					<p>Page in construction.
+					</p>
+				</header>
+				<figure id="imgExample"></figure>
+				<p>Page in construction.
+				</p>
+			</article>
+			<section id="tips">
+			</section>
+		</main>
+	</div>
+	<?php include "footer.php";?>
+</body>
+</html>
