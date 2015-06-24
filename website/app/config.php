dd93fb282d0f11d4b67b9a9eec0555d557576f71
Waldeilson Santos <wwesantos@gmail.com>
2015-06-23 23:33:29 -0400

    CSS corrections
    CSS corrections

--- website/app/config.php
+++ website/app/config.php
@@ -1,24 +1,19 @@
 <?php
 return array(
     'mode' => 'development', // or development
-    
     'default_lang' => 'en',
-    
     'development' => [
         'log.enable' => false,
         'debug' => true
     ],
-    
     'production' => [
         'log.enable' => true,
         'debug' => false
     ],
-    
     'view' => [
         'cache_path' => __DIR__ . '/../cache/',
         'templates_path' => __DIR__ . '/templates/'
     ],
-    
     'dbstring' => 'mysql:host=127.0.0.1;dbname=pronunciationDB2;charset=utf8',
     'dbusername' => 'root',
     'dbpassword' => ''
